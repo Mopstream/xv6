ce5625bb83615e3dec94cbd65017c46917a0786f
mopstream <golikov.andrei1@gmail.com>
2023-09-25 11:56:45 +0300

    labwork1

---
+++ user/pingpong.c
@@ -0,0 +1,43 @@
+#include "user.h"
+
+int main() {
+    int pipes[2][2];
+    if (pipe(pipes[0]) < 0) exit(0);
+    if (pipe(pipes[1]) < 0) exit(0);
+
+    int pid = fork();
+    if (pid < 0) {
+        exit(0);
+    } else if (pid == 0) {
+        int to_parent_pipe = pipes[1][1];
+        int from_parent_pipe = pipes[0][0];
+        close(pipes[1][0]);
+        close(pipes[0][1]);
+
+        char from_parent[4];
+        while (read(from_parent_pipe, from_parent, 4) == 0);
+        printf("%d: got %s\n", getpid(), from_parent);
+        write(to_parent_pipe, "pong", 4);
+
+        close(to_parent_pipe);
+        close(from_parent_pipe);
+
+        exit(0);
+
+    } else {
+        int from_child_pipe = pipes[1][0];
+        int to_child_pipe = pipes[0][1];
+        close(pipes[1][1]);
+        close(pipes[0][0]);
+
+        write(to_child_pipe, "ping", 4);
+        char from_child[4];
+        while (read(from_child_pipe, from_child, 4) == 0);
+        printf("%d: got %s\n", getpid(), from_child);
+
+        close(from_child_pipe);
+        close(to_child_pipe);
+
+        exit(0);
+    }
+}
